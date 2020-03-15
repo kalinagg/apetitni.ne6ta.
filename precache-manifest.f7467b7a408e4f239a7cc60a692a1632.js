82b9153deee90f3088618524c7141cf17f5820ba
Kalina <kalinagg@yahoo.de>
2020-03-15 18:13:04 +0100

    Updates

---
+++ precache-manifest.f7467b7a408e4f239a7cc60a692a1632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b4ccc39ebbceb131bb3bf984bcdda9",
+    "url": "/apetitni.ne6ta./index.html"
+  },
+  {
+    "revision": "da21770934d9eb33dcc2",
+    "url": "/apetitni.ne6ta./static/css/main.42d1944a.chunk.css"
+  },
+  {
+    "revision": "271ff52e437bb0d9fc47",
+    "url": "/apetitni.ne6ta./static/js/2.a5768a6e.chunk.js"
+  },
+  {
+    "revision": "da21770934d9eb33dcc2",
+    "url": "/apetitni.ne6ta./static/js/main.b039a9e9.chunk.js"
+  },
+  {
+    "revision": "53157e8e0b63021f24f7",
+    "url": "/apetitni.ne6ta./static/js/runtime-main.db4a9a2a.js"
+  },
+  {
+    "revision": "a68b7008f0cfa2d9168e8e4f306c3907",
+    "url": "/apetitni.ne6ta./static/media/logo.a68b7008.svg"
+  }
+]);
