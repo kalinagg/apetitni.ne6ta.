0e4298d8849695cfaf585ec8569de08dd1ba7b10
Kalina <kalinagg@yahoo.de>
2020-05-11 16:36:09 +0200

    Adding get function for the build app

--- server/index.js
+++ server/index.js
@@ -43,6 +43,10 @@ app.post('/upload', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+}); 
+
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );  
