747a8374244babbd0e547762622e60919377cecf
Kalina <kalinagg@yahoo.de>
2020-05-14 15:45:27 +0200

    Fixed upload image functionality

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.post('/upload', (req, res) => {
     const image = req.files.image;
     const ext = path.extname(image.name);
     const imagePath = 'img-food/' + 'img-' + uuidv4() + ext;
-
-    image.mv(imagePath, err => {
+ 
+    image.mv('client/build/' + imagePath, err => {
         if (err) {
             return res.status(500).send(err);
         }
